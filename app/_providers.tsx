3b7bb0f4e0bea857aea4c6a37948dc3d19cc322a
nthaw04 <thaonguyenngoc23122004@gmail.com>
2025-09-24 13:54:43 +0700

    feat: Update Base

--- app/_providers.tsx
+++ app/_providers.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import { ReactNode, useState } from "react";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createQueryClient } from "@/services/core/query-client";
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export default function Providers({ children }: { children: ReactNode }) {
-  const [qc] = useState(() => createQueryClient());
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={qc}>
-      {children}
-      {process.env.NODE_ENV === "development" && (
-        <ReactQueryDevtools initialIsOpen={false} />
-      )}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
